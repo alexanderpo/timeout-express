57eab2d05e270bcf0bf023dcaf30094dba0b0a47
alexanderpo <alexanderpometko@gmail.com>
2017-03-31 11:22:46 +0300

    Сlean up project

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,32 +1,9 @@
 import { Router } from 'express';
-import { routesTokenProtect } from '../middlewares/routesTokenProtect';
-import { authenticate, registration, updateInformation } from '../controllers/user';
-import {
-  createPost,
-  getAllPosts,
-  getSearchResult,
-  getPostsByAuthor,
-  likePost,
-} from '../controllers/post';
 
 const apiRouter = Router();
 
-apiRouter.post('/authenticate', authenticate );
-
-apiRouter.post('/registration', registration );
-
-apiRouter.use(routesTokenProtect);
-
-apiRouter.put('/profile/:id', updateInformation);
-
-apiRouter.get('/posts', getAllPosts);
-
-apiRouter.get('/posts-by-author/:id', getPostsByAuthor);
-
-apiRouter.post('/posts/search', getSearchResult);
-
-apiRouter.post('/post/create', createPost);
-
-apiRouter.post('/posts/:id/like', likePost);
+apiRouter.get('/', (req, res) => {
+  res.send('hello its  a root route');
+});
 
 export default apiRouter;

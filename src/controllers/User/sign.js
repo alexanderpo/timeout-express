412d2bef891da786fd5a19025de9815d53e91862
alexanderpo <alexanderpometko@gmail.com>
2017-04-03 21:01:31 +0300

    Implement sign in controlled method

--- src/controllers/User/sign.js
+++ src/controllers/User/sign.js
@@ -1,12 +1,53 @@
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
-import { signUpValidate } from '../../utils/validation';
-// import { config } from '../../config';
+import { signInValidate, signUpValidate } from '../../utils/validation';
+import { config } from '../../config';
 import { UserModel } from '../../models/user';
 
 export function signin (req, res) {
-  // TODO: implement signin controller
+  const { name, password } = req.body;
+  const values = { name, password };
+  const validateError = signInValidate(values);
+
+  if (_.isEmpty(validateError)) {
+    UserModel.findOne({ name: name }, (err, user) => {
+      if (err) {
+        res.status(500).json({
+          error: err,
+          message: 'Проблемы с подключением',
+        });
+      } else if (!user) {
+        res.status(200).json({
+          message: 'Данного пользователя не существует',
+        });
+      } else if (user) {
+        bcrypt.compare(password, user.hash, (err, isCompare) => {
+          if (isCompare) {
+            const token = jwt.sign(user, config.secret, {
+              expiresIn: '1440m',
+            });
+            res.status(200).json({
+              id: user._id,
+              name: user.name,
+              email: user.email,
+              hash: user.hash,
+              liked_posts: user.liked_posts,
+              token: token,
+            });
+          } else {
+            res.status(200).json({
+              message: 'Вы ввели неправельный пароль',
+            });
+          }
+        });
+      }
+    });
+  } else {
+    res.status(300).json({
+      error: validateError,
+    });
+  }
 }
 
 export function signup (req, res) {
@@ -17,12 +58,12 @@ export function signup (req, res) {
   if (_.isEmpty(validateError)) {
     UserModel.findOne({ name: name }, (err, user) => {
       if (err) {
-        res.status(500).send({
+        res.status(500).json({
           error: err,
           message: 'Проблемы с подключением',
         });
       } else if (user) {
-        res.status(200).send({
+        res.status(200).json({
           message: 'Такой пользователь уже существует',
         });
       } else if (!user) {
@@ -44,15 +85,15 @@ export function signup (req, res) {
             });
           }
           console.log('user saved successfully');
-          res.status(200).send({
+          res.status(200).json({
             message: 'Пользователь успешно создан',
           });
         });
       }
     });
   } else {
-    res.status(300).send({
-      error: validateError, // TODO: see more about http server codes
+    res.status(300).json({
+      error: validateError, // TODO: see more about server codes
     });
   }
 }
